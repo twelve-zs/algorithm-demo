7b2493dd81c92d9c136f2fe1ed17c631779e8351
臧胜 <zangshenghqv3@outlook.com>
2020-12-23 15:21:47 +0800

    init

---
+++ src/com/zangsheng/AlgorithmUtil.java
@@ -0,0 +1,35 @@
+package com.zangsheng;
+
+public class AlgorithmUtil {
+    public AlgorithmUtil(){
+
+    }
+
+    /**
+     * 数组元素交换
+     * @param arr 数组
+     * @param indexOne 元素1下标
+     * @param indexTwo 元素2下标
+     * @return 交换是否成功
+     */
+    public static boolean swap(int []arr,int indexOne,int indexTwo){
+        //数组为空交换失败
+        if (arr == null || arr.length == 0){
+            return false;
+        }
+        //下标小于0交换失败
+        if (indexOne<0||indexTwo<0){
+            return false;
+        }
+        //下表超过数组长度交换失败
+        if (arr.length-1<indexOne||arr.length-1<indexTwo){
+            return false;
+        }
+        //元素交换
+        int temp = arr[indexOne];
+        arr[indexOne] = arr[indexTwo];
+        arr[indexTwo] = temp;
+        return true;
+    }
+
+}
---
+++ src/com/zangsheng/Main.java
@@ -0,0 +1,10 @@
+package com.zangsheng;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
