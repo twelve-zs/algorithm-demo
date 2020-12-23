ef7b73c2716443601f619f8cb99df3802b58135b
臧胜 <zangshenghqv3@outlook.com>
2020-12-23 15:23:19 +0800

    全排列

--- src/com/zangsheng/Main.java
+++ src/com/zangsheng/Main.java
@@ -5,6 +5,18 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-
+        int []arr = new int[]{1,1};
+        Permutation perm = new Permutation();
+        List<String> ints = perm.noRepeatPermutation(arr);
+        for (String array:ints) {
+            System.out.println(array);
+        }
+        System.out.println(perm.getCount());
+        int []arr2 = new int[]{1,1,3,4,5,6};
+        List<String> intss = perm.noRepeatPermutation(arr2);
+        for (String array:intss) {
+            System.out.println(array);
+        }
+        System.out.println(perm.getCount());
     }
 }
---
+++ src/com/zangsheng/Permutation.java
@@ -0,0 +1,81 @@
+package com.zangsheng;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Permutation {
+
+    /**
+     * 全排列数量
+     */
+    private int count = 0;
+    /**
+     * 全排列各种组合
+     */
+    private List<String> listPermutation;
+
+
+    /**
+     * 获取包含重复的排列
+     * @param arr 待排列数组
+     * @return 排列集合
+     */
+    public List<String> repeatPermutation(int []arr){
+        this.count = 0;
+        this.listPermutation =  new ArrayList<>();
+        repeatPermutation(arr,0);
+        return this.listPermutation;
+    }
+    /**
+     * 获取不包含重复的排列
+     * @param arr 待排列数组
+     * @return 排列集合
+     */
+    public List<String> noRepeatPermutation(int []arr){
+        this.count = 0;
+        this.listPermutation =  new ArrayList<>();
+        noRepeatPermutation(arr,0);
+        return this.listPermutation;
+    }
+
+
+    private void repeatPermutation(int []arr,int index){
+        if (index==arr.length){
+            listPermutation.add(Arrays.toString(arr));
+            count++;
+        }
+        for(int i = index;i<arr.length;++i){
+            //交换元素
+            AlgorithmUtil.swap(arr,index,i);
+            //递归
+            repeatPermutation(arr,index+1);
+            //回复原排列顺序
+            AlgorithmUtil.swap(arr,index,i);
+        }
+    }
+    private void noRepeatPermutation(int []arr,int index){
+        if (index==arr.length){
+            listPermutation.add(Arrays.toString(arr));
+            count++;
+        }
+        for(int i = index;i<arr.length;++i){
+            if (i==index||arr[i]!=arr[index]){
+                //交换元素
+                AlgorithmUtil.swap(arr,index,i);
+                //递归
+                repeatPermutation(arr,index+1);
+                //回复原排列顺序
+                AlgorithmUtil.swap(arr,index,i);
+            }
+        }
+    }
+    /**
+     * 获取排列组合的个数
+     * @return
+     */
+    public int getCount() {
+        return count;
+    }
+
+}
