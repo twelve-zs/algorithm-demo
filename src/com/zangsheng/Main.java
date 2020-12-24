b52d1756a29129cab3ae4c17652866ea21fe0875
臧胜 <zangshenghqv3@outlook.com>
2020-12-24 22:27:41 +0800

    更新

--- src/com/zangsheng/Main.java
+++ src/com/zangsheng/Main.java
@@ -3,7 +3,8 @@ package com.zangsheng;
 import java.util.*;
 
 public class Main {
-  public static   ArrayList<String> list =new ArrayList<>();
+    public static ArrayList<String> list = new ArrayList<>();
+
     public static void main(String[] args) {
         //全排列demo
 //        int []arr = new int[]{1,2,3};
@@ -26,8 +27,16 @@ public class Main {
 //        GenerateParenthesis parenthesis = new GenerateParenthesis();
 //        ArrayList<String> strings = parenthesis.generateParenthesis(10);
 //       System.out.println(strings);
-    }
 
+        //二叉树排序
+        int[] arr = new int[]{1, 5, 7, 64, 8, 79, 79, 51, 35, 789, 78, 11, 85, 0, 748, 1874, 748, 748, 17489, 1748, 1748, 17, 777, 123, 4, 0, 1, 3, 74, 9, 8, 5, 41, 8};
+//        TreeSort treeSort = new TreeSort();
+//        int[] ascArr = treeSort.getAscArr(arr);
+//        int[] desArr = treeSort.getDesArr(arr);
+//        System.out.println( Arrays.toString(ascArr));
+//        System.out.println(Arrays.toString(desArr));
+
+    }
 
 
 }
